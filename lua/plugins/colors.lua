a15ce2d47d6b585ed1293be365584860bb13cb42
kiqkuk <kiqkuk@gmail.com>
2026-04-14 04:50:35 +0700

    ui: customize statusline

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -26,14 +26,18 @@ local cp = require("catppuccin.palettes").get_palette("mocha")
 local set_hl = vim.api.nvim_set_hl
 
 -- mode colors
-set_hl(0, "StatusNormal", { fg = cp.base, bg = cp.lavender, bold = true })
-set_hl(0, "StatusInsert", { fg = cp.base, bg = cp.green, bold = true })
-set_hl(0, "StatusVisual", { fg = cp.base, bg = cp.peach, bold = true })
-set_hl(0, "StatusReplace", { fg = cp.base, bg = cp.yellow, bold = true })
-set_hl(0, "StatusCmd", { fg = cp.base, bg = cp.mauve, bold = true })
-set_hl(0, "StatusTerminal", { fg = cp.base, bg = cp.flamingo, bold = true })
+set_hl(0, "StatusNormal", { fg = cp.crust, bg = cp.lavender, bold = true })
+set_hl(0, "StatusInsert", { fg = cp.crust, bg = cp.green, bold = true })
+set_hl(0, "StatusVisual", { fg = cp.crust, bg = cp.peach, bold = true })
+set_hl(0, "StatusReplace", { fg = cp.crust, bg = cp.red, bold = true })
+set_hl(0, "StatusCmd", { fg = cp.crust, bg = cp.yellow, bold = true })
+set_hl(0, "StatusTerminal", { fg = cp.crust, bg = cp.flamingo, bold = true })
 
 -- other colors
-set_hl(0, "StatusPath", { fg = cp.text, bg = cp.base })
-set_hl(0, "StatusDiag", { fg = cp.text, bg = cp.mantle })
-set_hl(0, "StatusFiletype", { fg = cp.mauve, bg = cp.crust })
+set_hl(0, "StatusFile", { fg = cp.lavender, bg = cp.base, bold = true })
+set_hl(0, "StatusDir", { fg = cp.overlay0, bg = cp.base })
+set_hl(0, "StatusMod", { fg = cp.text, bg = cp.base })
+set_hl(0, "StatusRO", { fg = cp.red, bg = cp.base })
+set_hl(0, "StatusDiag", { fg = cp.text, bg = cp.base })
+set_hl(0, "StatusFt", { fg = cp.peach, bg = cp.base, bold = true })
+set_hl(0, "StatusPos", { fg = cp.overlay0, bg = cp.base, bold = true })
--- lua/statusline.lua
+++ lua/statusline.lua
@@ -20,21 +20,35 @@ function M.render()
 
   local mode_str = string.format("%s %s ", highlight, label)
 
-  local file_str = "%#StatusPath# %f %m %r "
+  local file_str = "%#StatusFile# %t "
+
+  local dir = vim.fn.expand("%:~:h")
+  if dir == "." then dir = "~" end
+  local dir_str = string.format("%%#StatusDir# %s/ ", dir)
+
+  local mod_str = "%#StatusMod#%m "
+  local ro_str = "%#StatusRO#%r "
 
   local diag = vim.diagnostic.status()
   local diag_str = (diag and diag ~= "") and ("%#StatusDiag# " .. diag .. " ") or ""
 
   local ft = vim.bo.filetype:upper()
+  local ft_str = string.format("%%#StatusFt# %s ", ft )
+
   local location = "%l:%c"
-  local ft_str = string.format("%%#StatusFiletype# %s [%s] ", ft, location)
+  local position = "%P"
+  local pos_str = string.format("%%#StatusPos# [%s] %s ", location, position)
 
   return table.concat({
     mode_str,
     file_str,
+    dir_str,
+    mod_str,
+    ro_str,
     "%=",
     diag_str,
-    ft_str
+    ft_str,
+    pos_str
   })
 end
 
