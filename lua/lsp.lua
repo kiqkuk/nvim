d4f9039b84951a7349ee69dafc1c97e7e62a7a33
kiqkuk <kiqkuk@gmail.com>
2026-04-13 22:07:16 +0700

    feat: add lsp and autocomplete with new 0.12 API

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -2,8 +2,19 @@
 
 local lsp = vim.lsp
 
-lsp.enable("clangd")
-lsp.enable("lua_ls", {
+lsp.config.clangd = {
+  cmd = {
+    "clangd",
+    "--offset-encoding=utf-16",
+  },
+  filetypes = { "c", "cpp", "objc", "objcpp" },
+  root_dir = vim.fs.root(0, { ".git", "compile_commands.json", "build", "Makefile" }),
+}
+
+lsp.config.lua_ls = {
+  cmd = { "lua-language-server" },
+  filetypes = { "lua" },
+  root_dir = vim.fs.root(0, { ".git", "init.lua" }),
   settings = {
     Lua = {
       diagnostics = {
@@ -11,6 +22,28 @@ lsp.enable("lua_ls", {
       },
     },
   },
-})
+}
 
-vim.bo.omnifunc = "v:lua.vim.lsp.omnifunc"
+lsp.config.pyright = {
+  cmd = {
+    "pyright-langserver",
+    "--stdio",
+  },
+  filetypes = { "python" },
+  root_dir = vim.fs.root(0, { ".git", "setup.py", "pyproject.toml", "requirements.txt" }),
+}
+
+lsp.enable("clangd")
+lsp.enable("lua_ls")
+lsp.enable("pyright")
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(ev)
+    local client = lsp.get_client_by_id(ev.data.client_id)
+    if client and client:supports_method('textDocument/completion') then
+      vim.lsp.completion.enable(true, client.id)
+    end
+
+    vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+  end,
+})
--- lua/options.lua
+++ lua/options.lua
@@ -4,6 +4,7 @@ local opt = vim.opt
 
 opt.number = true
 opt.relativenumber = true
+opt.showmode = false
 
 opt.expandtab = true
 opt.shiftwidth = 2
@@ -11,7 +12,10 @@ opt.tabstop = 2
 
 opt.termguicolors = true
 
-opt.completeopt = { "menuone", "popup", "noinsert", "nearest" }
+opt.completeopt = { "menuone", "popup", "noinsert", "noselect" }
+opt.autocomplete = true
+opt.autocompletedelay = 100
+opt.autocompletetimeout = 80
 
 -- popup menu
 opt.pumborder = "rounded"
@@ -21,3 +25,6 @@ opt.updatetime = 250
 
 -- sane undo
 opt.undofile = true
+
+-- statusline
+opt.statusline = "%!v:lua.require('statusline').render()"
